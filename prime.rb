ab4f8c34b3025c75876ae730619f97edc980027a
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-11 23:35:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+# Takes an integer arg, returns a boolean based on whether 
+# it's a prime number or not.
+
+# Prime number = a whole number greater than 1 whose only 
+# factors are 1 and itself
+
+
