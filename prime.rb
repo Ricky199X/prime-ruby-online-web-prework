19510c29060887c95646c2a647232de724d99896
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-12 00:03:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 # Takes an integer arg, returns a boolean based on whether 
 # it's a prime number or not.
 
-
-
-def prime?(number)
-  (2..number - 1).each do |num|
-  if num == 0
-    false
-    end
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  else (2..n/2).none? do |x|
+    n % x == 0
   end
-    true
+  end
 end
