3ed568141235e34438b631e1abd0d44591e21ae2
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-12 00:00:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,6 @@
 # Takes an integer arg, returns a boolean based on whether 
 # it's a prime number or not.
 
-# Prime number = a whole number greater than 1 whose only 
-# factors are 1 and itself
 
 
 def prime?(number)
